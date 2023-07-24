878440857eae15e124fbbdafb927e3b06578119a
Özkan Özlü <ozkanozlu@hotmail.com>
2023-07-24 14:00:11 +0300

    24 Temmuz 2023 tarihli calismalar

---
+++ 17_while.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    int i = 0;
+
+    while (i < 10) 
+    {
+        printf("%d\n", i);
+        i++; //kontrol ifadesi döngü içinde değiştirilmesi gerekir.
+    }
+
+    return 0;
+}
